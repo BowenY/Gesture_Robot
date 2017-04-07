212f592d9b970cb6f65f432ea1a24b96242c5d46
BowenY <bzy0014@tigermail.auburn.edu>
2017-04-07 16:17:08 -0500

    sonar_arduino

--- Gen2_Platforms/Arduino/Gen2_Arduino/Gen2_Arduino.ino
+++ Gen2_Platforms/Arduino/Gen2_Arduino/Gen2_Arduino.ino
@@ -2,7 +2,8 @@
 #include <ros.h>
 #include <std_msgs/Int16.h>
 #include <std_msgs/Float32.h> 
-
+#include <ros/time.h>
+#include <sensor_msgs/Range.h>
 //Gen2 Includes
 #include <gen2_motor.h>
 #include <gen2_encoder.h>
@@ -32,7 +33,7 @@ gen2_encoder eleft(encoderLeft_CH1, encoderLeft_CH2, true);
 gen2_motor m(motorRight_Enable, motorRight_I1, motorRight_I2,motorLeft_Enable, motorLeft_I1, motorLeft_I2);
 
 //Publish/Subscribe Timer durations
-long pub_duration = 10;   //Rate in ms to publish encoder topics 
+long pub_duration = 50;   //Rate in ms to publish encoder topics 
 unsigned long pub_timer = 0;
 
 //ROS subscriber Callback functions
@@ -69,13 +70,37 @@ void r_cmd_Cb(const std_msgs::Float32& r_motor_cmd)
 //Create ROS nodehandle and define messages
 ros::NodeHandle  nh;
 std_msgs::Int16 l_enc,r_enc, gyro_raw;
+sensor_msgs::Range range_msg;
+ros::Publisher pub_range1("/ultrasound", &range_msg); //first sonar sensor
 ros::Publisher lwheel("lwheel", &l_enc);  //left encoder ticks
 ros::Publisher rwheel("rwheel", &r_enc);  //right encoder ticks
 ros::Publisher gyro_val("gyro_val", &gyro_raw); //raw gyro value
 ros::Subscriber<std_msgs::Float32> l_cmd_sub("l_motor_cmd", &l_cmd_Cb);
 ros::Subscriber<std_msgs::Float32> r_cmd_sub("r_motor_cmd", &r_cmd_Cb);
 
+char frameid[] = "/ultrasound";
 
+long duration;
+
+float getRange_Ultrasound(int pin_num)
+{
+  // The Ping))) i triggered by a HIGH pulse of 2 or moe microseconds.
+  // Give a short LOW pulse beforehand to ensure a clean HIGH pulse;
+  pinMode(pin_num, OUTPUT);
+  digitalWrite(pin_num, LOW);
+  delayMicroseconds(2);
+  digitalWrite(pin_num, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(pin_num, LOW);
+  
+  // same pin to receive
+  pinMode(pin_num, INPUT);
+  duration = pulseIn(pin_num, HIGH);
+  
+  // convert time to distance
+  return duration/58; // duration/29/2, centimeters
+}
+  
 void setup()
 {
   //Interrupt Initialization for Encoder Counting
@@ -87,6 +112,7 @@ void setup()
   nh.advertise(lwheel);
   nh.advertise(rwheel);
   nh.advertise(gyro_val);
+  nh.advertise(pub_range1);
   nh.subscribe(l_cmd_sub);
   nh.subscribe(r_cmd_sub);
   
@@ -95,9 +121,16 @@ void setup()
   TCCR3B &= ~myEraser; 
   TCCR3B |= myPrescaler;
   
+  range_msg.radiation_type = sensor_msgs::Range::ULTRASOUND;
+  range_msg.header.frame_id = frameid;
+  range_msg.field_of_view = 0.1; //fake
+  range_msg.min_range = 0.0;
+  range_msg.max_range = 2.0;
   //Start serial interface
   //Serial.begin(9600);
 }
+
+long range_time;
 
 void loop()
 {
@@ -105,6 +138,9 @@ void loop()
   if (millis() > pub_timer)
   {
     pub_timer = millis() + pub_duration;
+    range_msg.range = getRange_Ultrasound(52);
+    range_msg.header.stamp = nh.now();
+    pub_range1.publish(&range_msg);
     r_enc.data = eright.totaldistance();
     l_enc.data = eleft.totaldistance();
     gyro_raw.data = analogRead(gyro_pin);
@@ -145,5 +181,5 @@ void motor_test()
   m.right_mspeed(75);
   m.right_backward();
   m.left_backward();
- 
+ m.left_backward();
 }
